2ea62c77f4d52e9f8e134d884c261209433acff7
Oleksandr Sachuk <ovsachuk@gmail.com>
2018-06-15 16:19:34 +0300

    Fix SSL

--- download_image.py
+++ download_image.py
@@ -1,5 +1,11 @@
+import os, ssl
 import random
 import urllib.request
+
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+        getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
 
 
 def download_image(url):
@@ -8,4 +14,5 @@ def download_image(url):
     urllib.request.urlretrieve(url, full_name)
 
 
-download_image('https://res.cloudinary.com/jerrick/image/upload/f_auto,fl_progressive,q_auto,c_fit,w_1140/wxknmnzpjpjwbrpdbybw')
+download_image(
+    'https://res.cloudinary.com/jerrick/image/upload/f_auto,fl_progressive,q_auto,c_fit,w_1140/wxknmnzpjpjwbrpdbybw')
