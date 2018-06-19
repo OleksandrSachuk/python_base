85ad984a30d40e229c48dd2d8440821660442656
Oleksandr Sachuk <ovsachuk@gmail.com>
2018-06-19 11:45:14 +0300

    Add __init__ to class example

--- class.py
+++ class.py
@@ -1,6 +1,9 @@
 class Enemy:
     life = 3
 
+    def __init__(self, x):
+        self.energy = x
+
     def attack(self):
         print('ouch!')
         self.life -= 1
@@ -11,9 +14,14 @@ class Enemy:
         else:
             print(str(self.life) + " life left")
 
+    def get_energy(self):
+        print(str(self.energy) + " energy")
 
-enemy1 = Enemy()
+
+enemy1 = Enemy(5)
 
 enemy1.attack()
 enemy1.attack()
 enemy1.checkLife()
+enemy1.checkLife()
+enemy1.get_energy()
